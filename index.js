dba2e8e2c819cc8f5a0e10cc6c6c06b77eafedc0
masibaon <30910115+masibaon@users.noreply.github.com>
2017-08-10 22:36:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ restService.use(bodyParser.urlencoded({
   
   restService.use(bodyParser.json());
   
-  restService.post('/echo', function(reeq, res) {
+  restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like there is an issue. Seack again."
     return res.json({
       speech: speech,
